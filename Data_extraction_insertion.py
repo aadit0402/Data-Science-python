f253956eac71f63ec138a4d9f0451f7a36f19490
Aditya Upadhyay <aadit0402@gmail.com>
2017-10-09 20:55:23 +0530

    writing data to csv file using pandas

--- Data_extraction_insertion.py
+++ Data_extraction_insertion.py
@@ -9,7 +9,17 @@ Created on Mon Oct  9 20:39:27 2017
 import numpy as np
 import pandas as pd
 
+#accessing data from a csv file
 d = pd.read_csv('/home/aditya/Data-Science-python/Student_Weight_Status_Category_Reporting_Results__Beginning_2010.csv')
 
 print d[0:5]['AREA NAME']
+
+#write a data to .csv file
+
+d = {'c1':pd.Series(['a', 'b', 'c']), 'c2':pd.Series([1,2., 3.,4.])}
+
+df = pd.DataFrame(d)
+df.to_csv('/home/aditya/Data-Science-python/sample_data.csv') 
+
+
 
