0eb2a6acc2c56028df93436a52d90ece6fea1666
Aditya Upadhyay <aadit0402@gmail.com>
2017-10-09 20:19:16 +0530

    Dataframe generation using pandas

--- Data_Analysis_panda.py
+++ Data_Analysis_panda.py
@@ -20,3 +20,21 @@ print pd.Series(np.random.randn(5))
 # we can customize the index
 print pd.Series(np.random.randn(5), index = ['a', 'b', 'c', 'd', 'e'])
 
+# Series from a python dict 
+
+d = {'a':10, 'b':20, 'c':30, 'd':40, 'e':50}
+print pd.Series(d)
+
+'''DataFrame is a 2d structure with columns that can be of different datatypes. (table)'''
+'''A dataFrame can be formed from following data structure a numpy array, list, dicts, series, a 2d numpy array'''
+#using dict
+
+d = {'c1':pd.Series(['a', 'b', 'c']), 'c2':pd.Series([1,2., 3.,4.])} #here bydefault index 0,1,2 ... is generated but we can manually set the index
+df = pd.DataFrame(d)
+print df
+
+'''panel is a data structure that handles 3d data'''
+
+
+
+
