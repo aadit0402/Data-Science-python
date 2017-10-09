2db11fd4effd77e28aa6fd6588da17873ad01d6e
Aditya Upadhyay <aadit0402@gmail.com>
2017-10-09 19:20:37 +0530

    array operation

---
+++ array_operation.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  9 18:57:32 2017
+
+@author: aditya
+"""
+
+import numpy as np
+
+n_array = np.array([[0,1,2,3],[4,5,6,7],[8,9,10,11]])
+
+print ("output:2", n_array.ndim)
