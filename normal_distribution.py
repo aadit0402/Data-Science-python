a9169cdce47d6c508bc481df8fa0bfb003b29588
Aditya Upadhyay <aadit0402@gmail.com>
2017-10-23 16:23:50 +0530

    effect of increasing No. of Attempts on Distribution.

--- normal_distribution.py
+++ normal_distribution.py
@@ -1,11 +1,16 @@
 #normal distribution from binomial distribution
 
+#if the coin is flipped 6 times, then getting three heads has 
+#the maximum probability, whereas getting a single head or five head
+#has the least probability.
+#here we are increasing the number of attempts then the 
+#distribution will be like this:
 from scipy.stats import binom
 import matplotlib.pyplot as plt 
 
 fig, ax = plt.subplots(1,1)
-x = [0,1,2,3,4,5,6,7]
-n, p = 2, 0.4
+x = range(101)
+n, p = 100, 0.5
 rv = binom(n,p)
 ax.vlines(x, 0, rv.pmf(x), colors = 'k', linestyles = '-', lw = 1, label = 'probability')
 ax.legend(loc= 'best', frameon = False)
